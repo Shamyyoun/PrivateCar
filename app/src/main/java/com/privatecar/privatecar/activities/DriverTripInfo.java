be1eac326175b166cf5abc0b96c503e123760d62
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-16 01:04:25 +0200

    DriverTripInfo Activity Setup

---
+++ app/src/main/java/com/privatecar/privatecar/activities/DriverTripInfo.java
@@ -0,0 +1,15 @@
+package com.privatecar.privatecar.activities;
+
+import android.os.Bundle;
+
+import com.privatecar.privatecar.R;
+
+public class DriverTripInfo extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_trip_info);
+    }
+
+}
