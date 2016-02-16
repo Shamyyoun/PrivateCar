79979d3b1d602d95d4c94b637221a480d3785d2b
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-16 03:41:06 +0200

    DriverCreditPaymentActivity setup

---
+++ app/src/main/java/com/privatecar/privatecar/activities/DriverCreditPaymentActivity.java
@@ -0,0 +1,14 @@
+package com.privatecar.privatecar.activities;
+
+import android.os.Bundle;
+
+import com.privatecar.privatecar.R;
+
+public class DriverCreditPaymentActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_credit_payment);
+    }
+}
