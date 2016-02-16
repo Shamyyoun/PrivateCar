8b911c6d16f837fb135cd620bf182044ee1426dc
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-16 03:30:21 +0200

    DriverCashPaymentActivity

---
+++ app/src/main/java/com/privatecar/privatecar/activities/DriverCashPaymentActivity.java
@@ -0,0 +1,18 @@
+package com.privatecar.privatecar.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.privatecar.privatecar.R;
+
+public class DriverCashPaymentActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_cash_payment);
+    }
+
+    public void onClick(View view) {
+    }
+}
