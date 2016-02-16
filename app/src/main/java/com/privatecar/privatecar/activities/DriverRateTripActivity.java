e3f1d61eb73e38d5be4ebb37c3e11f95ba364499
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-16 06:57:42 +0200

    DriverRateTripActivity setup

---
+++ app/src/main/java/com/privatecar/privatecar/activities/DriverRateTripActivity.java
@@ -0,0 +1,14 @@
+package com.privatecar.privatecar.activities;
+
+import android.os.Bundle;
+
+import com.privatecar.privatecar.R;
+
+public class DriverRateTripActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_rate_trip);
+    }
+}
--- app/src/main/java/com/privatecar/privatecar/activities/DriverTripInfoActivity.java
+++ app/src/main/java/com/privatecar/privatecar/activities/DriverTripInfoActivity.java
@@ -1,15 +1,21 @@
 package com.privatecar.privatecar.activities;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 import com.privatecar.privatecar.R;
 
 public class DriverTripInfoActivity extends BaseActivity {
 
+    RatingBar ratingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_trip_info);
+
+        ratingBar = (RatingBar) findViewById(R.id.rating_bar);
+
     }
 
 }
