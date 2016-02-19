4a6a3e99d621529f1c9c8e0ce385502a6c7e8a77
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-19 17:46:55 +0200

    DriverAddCarActivity setup

---
+++ app/src/main/java/com/privatecar/privatecar/activities/DriverAddCarActivity.java
@@ -0,0 +1,18 @@
+package com.privatecar.privatecar.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.privatecar.privatecar.R;
+
+public class DriverAddCarActivity extends BasicBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_add_car);
+    }
+
+    public void onClick(View view) {
+    }
+}
