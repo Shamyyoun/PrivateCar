3d79b75696b7e7219f0c8c550191bcdf65f14604
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-25 00:53:48 +0200

    Driver signup validation

--- app/src/main/java/com/privatecar/privatecar/Const.java
+++ app/src/main/java/com/privatecar/privatecar/Const.java
@@ -11,6 +11,52 @@ public class Const {
     public static final String SHARED_PREFERENCES_FILE_NAME = "private_car";
     public static final String APP_FILES_DIR = "/.private_car";
     public static final String PACKAGE_NAME = "com.privatecar.privatecar";
+    public static final int IMAGE_SIZE_USER = 400;
+    public static final int IMAGE_SIZE = 1024;
+
+    public static final int REQUEST_CAMERA_USER_PHOTO = 1001;
+    public static final int REQUEST_CAMERA_CAR_PHOTO = 1002;
+    public static final int REQUEST_CAMERA_ID_FRONT = 1003;
+    public static final int REQUEST_CAMERA_ID_BACK = 1004;
+    public static final int REQUEST_CAMERA_DRIVER_LICENCE_FRONT = 1005;
+    public static final int REQUEST_CAMERA_DRIVER_LICENCE_BACK = 1006;
+    public static final int REQUEST_CAMERA_CAR_LICENCE_FRONT = 1007;
+    public static final int REQUEST_CAMERA_CAR_LICENCE_BACK = 1008;
+    public static final int REQUEST_GALLERY_USER_PHOTO = 1009;
+    public static final int REQUEST_GALLERY_CAR_PHOTO = 1010;
+    public static final int REQUEST_GALLERY_ID_FRONT = 1011;
+    public static final int REQUEST_GALLERY_ID_BACK = 1012;
+    public static final int REQUEST_GALLERY_DRIVER_LICENCE_FRONT = 1013;
+    public static final int REQUEST_GALLERY_DRIVER_LICENCE_BACK = 1014;
+    public static final int REQUEST_GALLERY_CAR_LICENCE_FRONT = 1015;
+    public static final int REQUEST_GALLERY_CAR_LICENCE_BACK = 1016;
+    public static final int REQUEST_CROP_USER_PHOTO = 1017;
+    public static final int REQUEST_CROP_CAR_PHOTO = 1018;
+    public static final int REQUEST_CROP_ID_FRONT = 1019;
+    public static final int REQUEST_CROP_ID_BACK = 1020;
+    public static final int REQUEST_CROP_DRIVER_LICENCE_FRONT = 1021;
+    public static final int REQUEST_CROP_DRIVER_LICENCE_BACK = 1022;
+    public static final int REQUEST_CROP_CAR_LICENCE_FRONT = 1023;
+    public static final int REQUEST_CROP_CAR_LICENCE_BACK = 1024;
+
+
+    public static final String FILE_NAME_USER_PHOTO = "user_photo";
+    public static final String FILE_NAME_CROPPED_USER_PHOTO = "user_photo_cropped";
+    public static final String FILE_NAME_CAR_PHOTO = "car_photo";
+    public static final String FILE_NAME_CROPPED_CAR_PHOTO = "car_photo_cropped";
+    public static final String FILE_NAME_ID_FRONT = "id_front_photo";
+    public static final String FILE_NAME_CROPPED_ID_FRONT = "id_front_photo_cropped";
+    public static final String FILE_NAME_ID_BACK = "id_back_photo";
+    public static final String FILE_NAME_CROPPED_ID_BACK = "id_back_photo_cropped";
+    public static final String FILE_NAME_DRIVER_LICENCE_FRONT = "driver_licence_front_photo";
+    public static final String FILE_NAME_CROPPED_DRIVER_LICENCE_FRONT = "driver_licence_front_photo_cropped";
+    public static final String FILE_NAME_DRIVER_LICENCE_BACK = "driver_licence_back_photo";
+    public static final String FILE_NAME_CROPPED_DRIVER_LICENCE_BACK = "driver_licence_back_photo_cropped";
+    public static final String FILE_NAME_CAR_LICENCE_FRONT = "car_licence_front_photo";
+    public static final String FILE_NAME_CROPPED_CAR_LICENCE_FRONT = "car_licence_front_photo_cropped";
+    public static final String FILE_NAME_CAR_LICENCE_BACK = "car_licence_back_photo";
+    public static final String FILE_NAME_CROPPED_CAR_LICENCE_BACK = "car_licence_back_photo_cropped";
+
 
     //cached parameter names:---------------
     public static final String CACHE_CONFIGS = "configs_cache";
