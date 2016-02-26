8364c83c3a2afe2752f06ef9bb6a7ed297888f02
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-02-26 17:10:56 +0200

    update Const class

--- app/src/main/java/com/privatecar/privatecar/Const.java
+++ app/src/main/java/com/privatecar/privatecar/Const.java
@@ -71,13 +71,20 @@ public class Const {
     public static final String CACHE_CONFIGS = "configs_cache";
     public static final String CACHE_USER = "user_cache";
 
-    //messages:----
+    //messages (common):----
+    public static final String IMAGES_BASE_URL = "http://ec2-54-201-70-234.us-west-2.compute.amazonaws.com/api-repo-1/";
     public static final String MESSAGES_BASE_URL = "http://ec2-54-201-70-234.us-west-2.compute.amazonaws.com/api-repo-1/public/messages/";
     public static final String MESSAGE_STARTUP_CONFIG = "startupconfig";
     public static final String MESSAGE_USER_CONFIG = "userconfig";
-    public static final String MESSAGE_REGISTER_CUSTOMER = "regcustomer";
     public static final String MESSAGE_ACCESS_TOKEN = "accesstoken";
+
+    //messages (customer):----
+    public static final String MESSAGE_REGISTER_CUSTOMER = "regcustomer";
+
+    //messages (driver):----
     public static final String MESSAGE_DRIVER_ACCOUNT_DETAILS = "driveraccountdetails";
+    public static final String MESSAGE_DRIVER_SIGNUP = "regdriver";
+
 
     //parameters:----
     public static final String MSG_PARAM_CLIENT_ID = "client_id";
