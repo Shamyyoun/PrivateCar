0b0bca4c8a10bd43ffa0e015e8eaee5c57f8d6a1
Basim Alamuddin <basim.alamuddin@gmail.com>
2016-04-14 00:22:02 +0200

    added crashlytics dependencies

--- app/src/main/java/com/privatecar/privatecar/PrivateCar.java
+++ app/src/main/java/com/privatecar/privatecar/PrivateCar.java
@@ -2,7 +2,10 @@ package com.privatecar.privatecar;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.FacebookSdk;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by basim on 21/1/16.
@@ -11,7 +14,7 @@ public class PrivateCar extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
         FacebookSdk.sdkInitialize(getApplicationContext());
     }
 }
